34d5c75e3135faae02c6b49a0e9d65014d179076
Quyen <dgiaquyen40@gmail.com>
2023-02-24 23:09:57 +0700

    BT JS capstone

--- js/index.js
+++ js/index.js
@@ -1,32 +1,32 @@
-getProducts();
+// getProducts();
 
-function getProducts() {
-    axios({
-        method: "GET",
-        url: "https://63f716f9e8a73b486af07468.mockapi.io/api/CartProduct",
-    }).then((response) => {
-        console.log(response.data);
-        renderProducts(response.data);
-    }).catch((error) => {
-        alert("API Error");
-    });
-}
+// function getProducts() {
+//     axios({
+//         method: "GET",
+//         url: "https://63f716f9e8a73b486af07468.mockapi.io/api/CartProduct",
+//     }).then((response) => {
+//         console.log(response.data);
+//         renderProducts(response.data);
+//     }).catch((error) => {
+//         alert("API Error");
+//     });
+// }
 
-function renderProducts(products) {
-    let html = products.reduce((result, product,index) => {
-        return (result + `
-        <tr class="test">
-            <td>${index + 1}</td>
-            <td>${product.name}</td>
-            <td>${product.screen}</td>
-            <td>${product.backCamera}</td>
-            <td>${product.frontCamera}</td>
-        </tr>
-        `
-        );
-    },"");
-    document.getElementById("overlaySP").innerHTML = html;
-}
+// function renderProducts(products) {
+//     let html = products.reduce((result, product,index) => {
+//         return (result + `
+//         <tr class="test">
+//             <td>${index + 1}</td>
+//             <td>${product.name}</td>
+//             <td>${product.screen}</td>
+//             <td>${product.backCamera}</td>
+//             <td>${product.frontCamera}</td>
+//         </tr>
+//         `
+//         );
+//     },"");
+//     document.getElementById("overlaySP").innerHTML = html;
+// }
 
 
 
