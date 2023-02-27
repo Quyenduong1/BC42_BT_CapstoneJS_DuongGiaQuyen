c4e8cdd4cd648e9b51623d5fbe78cb15d4fe474c
Quyen <dgiaquyen40@gmail.com>
2023-02-27 19:05:31 +0700

    ...

--- js/index.js
+++ js/index.js
@@ -1,28 +1,14 @@
-
-// function getProducts() {
-//     axios({
-//         method: "GET",
-//         url: "https://63f716f9e8a73b486af07468.mockapi.io/api/CartProduct",
-//     }).then((response) => {
-//         console.log(response.data);
-//         renderProducts(response.data);
-//     }).catch((error) => {
-//         alert("API Error");
-//     });
+// let removeItemButton = document.getElementsByClassName('btn-danger')
+// console.log(removeItemButton);
+// for (let i = 0; i < removeItemButton.length; i++){
+//     let button = removeItemButton[i]
+//     button.addEventListener('click', function (event) {
+//         let buttonClicked = event.target
+//         buttonClicked.parentElement.parentElement.remove()
+//         updateCartTotal();
+//     })
 // }
 
-// function renderProducts(products) {
-//     let html = products.reduce((result, product,index) => {
-//         return (result + `
-//         <tr class="test">
-//             <td>${index + 1}</td>
-//             <td>${product.name}</td>
-//             <td>${product.screen}</td>
-//             <td>${product.backCamera}</td>
-//             <td>${product.frontCamera}</td>
-//         </tr>
-//         `
-//         );
-//     },"");
-//     document.getElementById("overlaySP").innerHTML = html;
+// function updateCartTotal() {
+//     let 
 // }
